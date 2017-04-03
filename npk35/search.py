f192e4858cea2bf2cc5fe036d981800df515da3b
gav <gavt45@bk.ru>
2017-04-03 08:05:22 -0400

    Converted search to class and removed unneeded files.

--- npk35/search.py
+++ npk35/search.py
@@ -1,81 +1,70 @@
-
 # coding: utf-8
-
-# In[3]:
-
-import os
-import re
-import json
-import pymorphy2
-from math import log
+import re, json, pymorphy2
 from sklearn.feature_extraction.text import CountVectorizer
 
-morph = pymorphy2.MorphAnalyzer()
-vectorizer = CountVectorizer()
 
-def lemmatize(token):
-    try: 
-        gram_info = morph.parse(token)
-        return gram_info[0].normal_form
-    except:
-        return token
+class search():
+    morph = pymorphy2.MorphAnalyzer()
+    vectorizer = CountVectorizer()
+    jsonFile = None
+    defaultPath = None
 
+    def __init__(self, jsonFile, defaultpath):
+        print("Using searcher; json file is: " + jsonFile + " & default path is: " + defaultpath)
+        self.jsonFile = jsonFile
+        self.defaultPath = defaultpath
 
-def clean(text):
-    text = re.sub('[!"#$%&\'()*+,\../:;<=>?@[\\]^_`{|}~«–»—№<>]+', ' ', text)
-    words = text.lower().split()
-    tokens = [lemmatize(w) for w in words if len(w) > 2 and not w.isdigit() and not w.split('-')[0].isdigit()]
-    return tokens
+    def lemmatize(self, token):
+        try:
+            gram_info = self.morph.parse(token)
+            return gram_info[0].normal_form
+        except:
+            return token
 
+    def clean(self, text):
+        text = re.sub('[!"#$%&\'()*+,\../:;<=>?@[\\]^_`{|}~«–»—№<>]+', ' ', text)
+        words = text.lower().split()
+        tokens = [self.lemmatize(w) for w in words if len(w) > 2 and not w.isdigit() and not w.split('-')[0].isdigit()]
+        return tokens
 
-def get_documents(query, invertedIndex):
-    query = ' '.join(clean(query))
-    if len(query.split()) == 1:
-        if query in invertedIndex.keys():
-            return [filename for filename in invertedIndex[query].keys()]
+    def get_documents(self, query, invertedIndex):
+        query = ' '.join(self.clean(query))
+        if len(query.split()) == 1:
+            if query in invertedIndex.keys():
+                return [filename for filename in invertedIndex[query].keys()]
+            else:
+                return []
         else:
-            return []
-    else:
-        result = []
-        for word in query.split():
-            if word in invertedIndex.keys():
-                result.append([filename for filename in invertedIndex[word].keys()])                
-        return list(set(result[0]).intersection(*result))
+            result = []
+            for word in query.split():
+                if word in invertedIndex.keys():
+                    result.append([filename for filename in invertedIndex[word].keys()])
+            return list(set(result[0]).intersection(*result))
 
+    def main(self, query):
+        with open(self.defaultPath + self.jsonFile) as f:
+            invertedIndex, idf, file2words = json.load(f)
 
-def main(query):
-    with open('index_idf_file2words.json') as f:
-        invertedIndex, idf, file2words = json.load(f)
+        docs = self.get_documents(query, invertedIndex)
+        clean_docs = [' '.join(file2words[doc]) for doc in docs]
 
-    docs = get_documents(query, invertedIndex)
-    clean_docs = [' '.join(file2words[doc]) for doc in docs]
+        raw_v = self.vectorizer.fit_transform(clean_docs)
+        raw_vectors = raw_v.toarray()
+        raw_vectors = [list(v) for v in raw_vectors]
+        vectors = dict(zip(docs, raw_vectors))
+        vocab = self.vectorizer.get_feature_names()
 
-    raw_v = vectorizer.fit_transform(clean_docs)
-    raw_vectors = raw_v.toarray() 
-    raw_vectors = [list(v) for v in raw_vectors]   
-    vectors = dict(zip(docs, raw_vectors))
-    vocab = vectorizer.get_feature_names()
+        idf_vectors = {}
+        for doc in vectors:
+            idf_vectors[doc] = [tf * idf[vocab[i]] for i, tf in enumerate(vectors[doc])]
 
-    idf_vectors = {}
-    for doc in vectors:
-        idf_vectors[doc] = [tf*idf[vocab[i]] for i, tf in enumerate(vectors[doc])]
+        query_vector = self.vectorizer.transform([query]).toarray()
+        query_vector = list(query_vector[0])
 
-    query_vector = vectorizer.transform([query]).toarray() 
-    query_vector = list(query_vector[0])
+        cos_sim = {}
+        for doc in docs:
+            cos_sim[doc] = sum([x * y for x, y in zip(idf_vectors[doc], query_vector)])
 
-    cos_sim = {}
-    for doc in docs:
-        cos_sim[doc] = sum([x*y for x,y in zip(idf_vectors[doc], query_vector)])
+        ranked_docs = sorted(cos_sim, key=lambda x: x[1])[::-1]
 
-    ranked_docs = sorted(cos_sim, key=lambda x: x[1])[::-1]
-
-    return ranked_docs
-
-
-print(main('ехал кот')) 
-
-
-# In[ ]:
-
-
-
+        return ranked_docs
